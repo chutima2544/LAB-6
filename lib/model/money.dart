e113e86ff9c1a19c8df101692395d4c17b36ce8f
chutima <chutimamaa2544@gmail.com>
2022-08-15 16:33:05 +0700

    Create class

---
+++ lib/model/money.dart
@@ -0,0 +1,8 @@
+class Money{
+  String list;
+  String revenue;
+  String expenses;
+  String remain;
+
+  Money({required this.list,required this.revenue,required this.expenses,required this.remain});
+}
